8a13ff813570a1bf4a46fffdef0bcac59aa76252
Jordon de Hoog <jordon.dehoog@roomroster.com>
2018-08-14 22:23:14 -0400

    feat(ncaheader): add model for ncaheader

---
+++ src/files/parser/models/NCAHeader.ts
@@ -0,0 +1,32 @@
+import { read64LEFloat } from "../../../util/buffer";
+
+export class NCAHeader {
+  public magic: string;
+  public titleID: number;
+  public sdkVersion1: number;
+  public sdkVersion2: number;
+  public sdkVersion3: number;
+  public sdkVersion4: number;
+  public masterKeyRev: number;
+
+  constructor(bytes: Buffer) {
+    this.magic = bytes.toString("utf8", 512, 516);
+    this.titleID = read64LEFloat(bytes, 528);
+    this.sdkVersion1 = bytes[540];
+    this.sdkVersion2 = bytes[541];
+    this.sdkVersion3 = bytes[542];
+    this.sdkVersion4 = bytes[543];
+    this.masterKeyRev = bytes[544];
+  }
+
+  public toString() {
+    return `NCA - Header:
+    Magic: ${this.magic}
+    Title ID: ${this.titleID}
+    SDK Version 1: ${this.sdkVersion1}
+    SDK Version 2: ${this.sdkVersion2}
+    SDK Version 3: ${this.sdkVersion3}
+    SDK Version 4: ${this.sdkVersion4}
+    Master Key Rev: ${this.masterKeyRev}`;
+  }
+}
