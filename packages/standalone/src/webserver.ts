d296d0d7d12f67fd34fc2f96e49352645f3c52d3
Jordon de Hoog <jordon.dehoog@gmail.com>
2018-09-23 18:15:07 -0400

    chore(webserver): wip im too tired

--- packages/standalone/src/webserver.ts
+++ packages/standalone/src/webserver.ts
@@ -1,5 +1,8 @@
 import { IConfig } from "@nxbm/types";
 import Koa from "koa";
+import proxy from "koa-proxy";
+import serve from "koa-static";
+import { resolve } from "path";
 
 export function createWebServer(config: IConfig) {
   // TODO - Start the webserver
@@ -8,5 +11,25 @@ export function createWebServer(config: IConfig) {
   // Serve the public files
   // Setup CORS?
 
+  const { host, paths } = config;
   const server = new Koa();
+
+  server.use(
+    proxy({
+      host,
+      match: /^\/api\//
+    })
+  );
+
+  const webDir = resolve(__dirname, "public");
+  server.use(serve(webDir));
+
+  const mediaDir = resolve(paths!.data);
+  server.use(serve(mediaDir));
+
+  const port = config.port! + 1;
+  server.listen({
+    host,
+    port
+  });
 }
