184d36c892bb913e185db29f79f22d4faec30789
Jordon de Hoog <jordon.dehoog@gmail.com>
2018-09-23 17:12:32 -0400

    wip(webserver): start work on webserver module

--- packages/standalone/src/webserver.ts
+++ packages/standalone/src/webserver.ts
@@ -1,5 +1,12 @@
 import { IConfig } from "@nxbm/types";
+import Koa from "koa";
 
-export function startWebServer(config: IConfig) {
+export function createWebServer(config: IConfig) {
   // TODO - Start the webserver
+  // Use the proxy for any api requests
+  // https://stackoverflow.com/questions/44403320/forward-all-requests-in-in-node-js-koa-server-from-http-server-api-to-anothe
+  // Serve the public files
+  // Setup CORS?
+
+  const server = new Koa();
 }
